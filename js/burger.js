804b8f3d573a9c72359671ea8a11325c068aeac4
Nika <Dayanova27@gmail.com>
2024-09-07 11:43:10 +0500

    feat: add function for closing menu

--- js/burger.js
+++ js/burger.js
@@ -5,3 +5,10 @@ burger.addEventListener("click", function() {
     menu.classList.toggle("open");
 })
 
+menu.addEventListener("click", function(event) {
+    const isAnchorLink = event.target.closest("[data-anchor-link]");
+
+    if (isAnchorLink) {
+        menu.classList.remove("open");
+    }
+})
