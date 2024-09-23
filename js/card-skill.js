398a31abecae94852f709368b2ba2e88b2e732fb
Nika <Dayanova27@gmail.com>
2024-09-23 10:52:17 +0500

    feat: add function to close cards and cross

--- js/card-skill.js
+++ js/card-skill.js
@@ -2,8 +2,13 @@ const cardList = document.querySelector("#cardList");
 
 cardList.addEventListener("click", (e) => {
     const isCard = e.target.closest("[data-card]");
+    const isCross = e.target.closest("[data-cross]");
 
     if (isCard) {
-        isCard.classList.toggle("open");
+        isCard.classList.add("open");
+    }
+    
+    if (isCross) {
+        isCard.classList.remove("open");
     }
 })
