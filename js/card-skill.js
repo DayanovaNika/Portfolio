98cf68e038a24db2fd250d7ca5d039a307fc6d29
Nika <Dayanova27@gmail.com>
2024-10-05 11:42:17 +0500

    to-do: test card

--- js/card-skill.js
+++ js/card-skill.js
@@ -1,4 +1,5 @@
 const cardList = document.querySelector("#cardList");
+const fade = document.querySelector("#fade");
 
 cardList.addEventListener("click", (e) => {
     const isCard = e.target.closest("[data-card]");
@@ -6,9 +7,38 @@ cardList.addEventListener("click", (e) => {
 
     if (isCard) {
         isCard.classList.add("open");
+        fade.classList.add("open");
     }
-    
+
     if (isCross) {
         isCard.classList.remove("open");
+        fade.classList.remove("open");
     }
 })
+
+// const arr = ["апельсин", "apple", "banan", "avocado", "watermelon"]; 
+
+// arr.push("cucumber");
+// arr.unshift("mango");
+// arr.pop();
+// arr.shift();
+
+// arr.splice(2,1)
+// arr.splice(1,3, "огурец", "tomatoes", "potatoes");
+// arr.splice(3,0, "snikers") 
+// const copyArr = arr.slice(1,3)
+// console.log(copyArr);
+
+// let i = 0;
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let i = 99; i >= 30; i--) {
+//     console.log(i);
+// }
