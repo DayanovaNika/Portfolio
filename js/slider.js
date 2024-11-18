1c1f10f49414d2b3dffae1baa21516adfe6ce230
Nika <Dayanova27@gmail.com>
2024-11-18 10:29:18 +0500

    feat: add base structure for slider

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,31 @@
-// 1. найти в ДОМ заготовку
-// 2. создать slider-hidden
-// 3. создать track
-// 4. вставить слайды в track
-// 5. вставить track в slider-hidden
-// 6. slider-hidden вставить в слайдер
+// 1. найти в ДОМ заготовку +
+// 2. создать slider-hidden +
+// 3. создать track +
+// 4. вставить слайды в track +
+// 5. вставить track в slider-hidden +
+// 6. slider-hidden вставить в слайдер +
 // 7. создать стрелки в слайдер
 // 8. создать пагинацию в слайдере
 
 const initialSlider = (sliderOfSelector) => {
     const sliderElement = document.querySelector(sliderOfSelector);
-    console.log(sliderElement);
+    const sliderHidden = document.createElement("div");
+    sliderHidden.className = "slider-hidden";
+
+    const sliderTrack = document.createElement("div");
+    sliderTrack.className = "slider-track";
+
+    const slides = Array.from(sliderElement.children);
+
+    slides.forEach(slide => {
+        sliderTrack.insertAdjacentElement("beforeend", slide);
+    });
+
+    sliderHidden.insertAdjacentElement("beforeend", sliderTrack); 
+    sliderElement.insertAdjacentElement("beforeend", sliderHidden);
+    
+    
+
 }
 
 initialSlider("#slider");
