c79c1649fcf8c8a9cdd94f7612e62ba8985a6fec
Nika <Dayanova27@gmail.com>
2024-11-23 11:27:22 +0500

    feat: add structure for slider

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,4 @@
-// 1. найти в ДОМ заготовку +
-// 2. создать slider-hidden +
-// 3. создать track +
-// 4. вставить слайды в track +
-// 5. вставить track в slider-hidden +
-// 6. slider-hidden вставить в слайдер +
-// 7. создать стрелки в слайдер
-// 8. создать пагинацию в слайдере
-
+// декомпозировать initialSlider
 const initialSlider = (sliderOfSelector) => {
     const sliderElement = document.querySelector(sliderOfSelector);
     const sliderHidden = document.createElement("div");
